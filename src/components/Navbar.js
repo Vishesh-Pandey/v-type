a59c22d357783d5cce30eaede1398281325c32d4
Shaurya-Sehgal <ashish.shaurya.as@gmail.com>
2023-02-01 16:09:39 -0700

    added theme to Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./favicon_ico.png";
+import Darkmode from "./dark_mode.svg";
+import Lightmode from "./light_mode.svg";
 
 function Navbar() {
+  const [textTheme, setTextTheme] = useState("dark");
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = () => {
+    if (theme == "dark") {
+      setTheme("light");
+      setTextTheme("dark");
+    } else {
+      setTheme("dark");
+      setTextTheme("light");
+    }
+  };
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-secondary bg-opacity-25 shadow-sm">
+      <nav className={`navbar navbar-expand-lg bg-${theme} shadow-sm`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="/v-type">
             <img width="40px" src={logo} alt="" />
           </a>
-          <a className="navbar-brand" href="/v-type">
+          <a className={`navbar-brand text-${textTheme}`} href="/v-type">
             vType
           </a>
           <button
@@ -26,7 +41,11 @@ function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a
+                  className={`nav-link active text-${textTheme}`}
+                  aria-current="page"
+                  href="/"
+                >
                   Home
                 </a>
               </li>
@@ -34,7 +53,7 @@ function Navbar() {
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  className="nav-link active"
+                  className={`nav-link active text-${textTheme}`}
                   aria-current="page"
                   href="https://vishesh-pandey.github.io/v-images/"
                 >
@@ -45,7 +64,7 @@ function Navbar() {
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  className="nav-link active"
+                  className={`nav-link active text-${textTheme}`}
                   aria-current="page"
                   href="https://vishesh-pandey.github.io/v-notes/"
                 >
@@ -53,6 +72,17 @@ function Navbar() {
                 </a>
               </li>
             </ul>
+            <a href="#">
+              <img
+                src={theme == "light" ? Darkmode : Lightmode}
+                alt=""
+                onClick={changeTheme}
+                width={30}
+                data-bs-toggle="tooltip"
+                data-bs-placement="bottom"
+                data-bs-title="Tooltip on bottom"
+              />
+            </a>
           </div>
         </div>
       </nav>
